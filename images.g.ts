bd054f7ca0847ce5430c9998f00e4e4b163bf8c4
IanOstrom <10860817+IanOstrom@users.noreply.github.com>
2024-09-24 12:43:54 -0400

    added ball on paddle before launch

---
+++ images.g.ts
@@ -0,0 +1,45 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "muNQmWQ4)(yLD8VSXTQ}":
+            case "giant paddle":return img`
+ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
+ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
+9999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999
+9999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999
+9999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999
+ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
+ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
+`;
+            case "image1":
+            case "normal paddle":return img`
+f f f f f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f f f f f 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+f f f f f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f f f f f 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
--- main.ts
+++ main.ts
@@ -2,18 +2,26 @@ namespace SpriteKind {
     export const Ball = SpriteKind.create()
 }
 function advanceLevel () {
-    totalScoreNeeded += levelScoresNeeded[level]
-    tiles.setCurrentTilemap(levelMaps[level])
-    level += 1
-    game.splash("Level " + level)
-    sprites.destroyAllSpritesOfKind(SpriteKind.Ball)
-    sprites.destroyAllSpritesOfKind(SpriteKind.Player)
-    spawnPaddle()
-    spawnBall()
+    if (level > lastLevel) {
+        game.gameOver(true)
+    } else {
+        totalScoreNeeded += levelScoresNeeded[level]
+        tiles.setCurrentTilemap(levelMaps[level])
+        level += 1
+        game.splash("Level " + level)
+        sprites.destroyAllSpritesOfKind(SpriteKind.Ball)
+        sprites.destroyAllSpritesOfKind(SpriteKind.Player)
+        spawnPaddle()
+        spawnBall()
+    }
 }
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Ball, function (sprite, otherSprite) {
     bounceBall(otherSprite)
     otherSprite.y = sprite.top - 1
+})
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    ball.setVelocity(ballSpeed, ballSpeed * -1)
+    ballFrozen = false
 })
 function spawnBall () {
     ball = sprites.create(img`
@@ -22,9 +30,9 @@ function spawnBall () {
         1 1 1 1 
         1 1 1 1 
         `, SpriteKind.Ball)
-    ball.setPosition(73, 58)
-    ball.setVelocity(ballSpeed, ballSpeed)
     ball.setBounceOnWall(true)
+    ball.setPosition(paddle.x, paddle.top - 2)
+    ballFrozen = true
 }
 function bounceBall (ball: Sprite) {
     ballVx = randint(ballSpeed / 3, ballSpeed)
@@ -34,6 +42,11 @@ function bounceBall (ball: Sprite) {
     }
     ball.setVelocity(ballVx, ballVy)
 }
+scene.onOverlapTile(SpriteKind.Ball, assets.tile`myTile0`, function (sprite, location) {
+    tiles.setTileAt(location, assets.tile`transparency16`)
+    bounceBall(sprite)
+    info.changeScoreBy(1)
+})
 scene.onOverlapTile(SpriteKind.Ball, assets.tile`myTile1`, function (sprite, location) {
     bounceBall(sprite)
     pause(100)
@@ -41,43 +54,36 @@ scene.onOverlapTile(SpriteKind.Ball, assets.tile`myTile1`, function (sprite, loc
     info.changeScoreBy(1)
 })
 function spawnPaddle () {
-    paddle = sprites.create(img`
-        ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
-        ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
-        9999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999
-        9999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999
-        9999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999
-        ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
-        ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
-        `, SpriteKind.Player)
+    paddle = sprites.create(assets.image`normal paddle`, SpriteKind.Player)
     paddle.setPosition(80, 107)
     controller.moveSprite(paddle, paddleSpeed, 0)
     paddle.setStayInScreen(true)
 }
-scene.onOverlapTile(SpriteKind.Ball, assets.tile`myTile0`, function (sprite, location) {
-    tiles.setTileAt(location, assets.tile`transparency16`)
-    bounceBall(sprite)
-    info.changeScoreBy(1)
-})
-let paddle: Sprite = null
 let ballVy = 0
 let ballVx = 0
+let paddle: Sprite = null
+let ballFrozen = false
 let ball: Sprite = null
 let paddleSpeed = 0
 let ballSpeed = 0
+let lastLevel = 0
 let level = 0
 let levelScoresNeeded: number[] = []
 let levelMaps: tiles.TileMapData[] = []
 info.setScore(1)
 levelMaps = [tilemap`level2`, tilemap`level0`, tilemap`level9`]
-levelScoresNeeded = [20, 25, 25]
+levelScoresNeeded = [20, 20, 15]
 let totalScoreNeeded = 0
 level = 0
+lastLevel = levelScoresNeeded.length - 1
 ballSpeed = 800
 paddleSpeed = 150
 advanceLevel()
 game.onUpdate(function () {
     if (info.score() >= totalScoreNeeded) {
         advanceLevel()
+    }
+    if (ballFrozen) {
+        ball.x = paddle.x
     }
 })
